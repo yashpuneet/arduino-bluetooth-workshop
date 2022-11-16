1bfbd5daa817fea364176ffab6ef31798cde5a0e
yashpuneet <yash.puneet02@gmail.com>
2022-11-16 15:55:06 -0800

    Demo code uploaded

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
 /*
 This file contains the code to be uploaded to the arduino microcontroller. 
 It sets up bluetooth communication and communicates with the processing
@@ -5,13 +8,20 @@ application to send data to be displayed and to allow the application
 to control the Arduino outputs.
 
 Data Codes:
-0: All Off
-1: Led Off
-2: Led On
+200: All Off
+201: Led Off
+202: Led On
 */
+
+#define TxD 2
+
+#define RxD 3
+
+SoftwareSerial mySerial(RxD, TxD); // RX, TX for Bluetooth
 
 // Pin numbers of outputs connected to the Arduino
 int led = 13;
+Servo servo;
 
 // Pin numbers of the inputs connected to the Arduino
 
@@ -27,41 +37,104 @@ void setup()
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
 
+  servo.attach(9);
+  servo.write(90);
+
   //setup bluetooth serial communication
-  Serial.begin(38400);
+  Serial.begin(9600);
+  mySerial.begin(9600); // For Bluetooth
 }
 
 void loop() 
 {
-  //Check for serial data availability
+  /*//Check for serial data availability
   if(Serial.available() > 0)
   {
     data = Serial.read(); //Read serial port data and store it in data variable
   }
 
   //Return everything to default state
-  if(data == '0')
+  if(data == 'a')
   {
     Serial.println("Set to default");
     setDefault();
-    delay(500);      
+    //delay(500);      
   }
 
   //Turn the led off
-  if(data == '1')
+  if(data == 'c')
   {
     Serial.println("LED off");
     digitalWrite(led, LOW);
-    delay(500);            
+    //delay(500);            
   }
 
   //Turn the led on
-  if(data == '2')
+  if(data == 'b')
   {
     Serial.println("LED on");
     digitalWrite(led, HIGH);
-    delay(500);
-  }  
+    //delay(500);
+  }*/
+  
+
+
+boolean isValidInput; 
+do {  while ( !mySerial.available() ) ; // LOOP...
+
+  data = mySerial.read(); // Execute the option based on the character recieved
+
+  //Serial.print(data); // Print the character received to the IDE serial monitor
+
+  switch ( data ) {
+
+    case 'a': // You've entered a
+
+      // Do the code you need when 'a' is received.....
+      mySerial.println( "You've entered an 'a'" );
+      mySerial.println("Set to default");
+      setDefault();
+
+      isValidInput = true;
+
+      break;
+
+    case 'b': // You've entered b
+
+      // Do the code you need when 'a' is received.....
+      mySerial.println("LED on");
+      digitalWrite(led, HIGH);
+
+      isValidInput = true;
+
+      break;
+
+    case 'c':
+      mySerial.println("LED off");
+      digitalWrite(led, LOW);
+
+      break;
+
+    default:
+
+      // Do the code you need when any other character is received.....
+
+      //mySerial.println( "Error" );
+
+      if(0<=(int)data<=180)
+      {
+        servo.write((int)data);
+        mySerial.print((int)data);
+        delay(15);            
+      } 
+      isValidInput = false;
+
+      break;
+
+    }
+
+} while ( isValidInput == true ); // Repeat the loop
+    
 }
 
 
@@ -70,3 +143,4 @@ void setDefault()
 {
   digitalWrite(led, LOW);
 }
+
--- processing/processing.pde
+++ processing/processing.pde
@@ -4,12 +4,14 @@ import processing.serial.*;
 //Name communication interface with Arduino
 Serial Arduino;
 
+int xpos=90;
+
 //Application and Communication Setup
 void setup(){
   size(500, 500); //Size of the application screen
   
   //Attaches communication info to interface
-  Arduino = new Serial(this, "COM6", 38400); // Starts the serial communication
+  Arduino = new Serial(this, "COM7", 38400); // Starts the serial communication
   Arduino.bufferUntil('\n'); // Defines delimiter/stop character
 }
 
@@ -33,16 +35,22 @@ void draw(){
   
   //Action for when Default is pressed
   if(mousePressed && mouseX>100 && mouseX<400 && mouseY>40 && mouseY<90){
-    Arduino.write('0'); // Writes Default data code
+    Arduino.write('a'); // Writes Default data code
   }
   
   //Action for when LED ON is pressed
   if(mousePressed && mouseX>100 && mouseX<220 && mouseY>100 && mouseY<150){
-    Arduino.write('2'); // Writes LED On data code
+    Arduino.write('b'); // Writes LED On data code
   }
   
   //Action for when LED OFF is pressed
   if(mousePressed && mouseX>280 && mouseX<400 && mouseY>100 && mouseY<150){
-    Arduino.write('1'); // Writes LED On data code
+    Arduino.write('c'); // Writes LED Off data code
+  }
+  
+  //Servo Control
+  if(mouseY > 400) {
+    Arduino.write((int)180*(mouseX)/500);
+    delay(50);
   }
 }
